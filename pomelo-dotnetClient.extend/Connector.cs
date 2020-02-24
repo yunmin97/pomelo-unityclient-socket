7b1f0df60dba1897567e3e89401545f5b7ac03c7
yunmin <362279869@qq.com>
2020-02-24 22:27:03 +0800

    extend for unity.

---
+++ pomelo-dotnetClient.extend/Connector.cs
@@ -0,0 +1,161 @@
+﻿/*
+ * @Author: yunmin
+ * @Email: 362279869@qq.com
+ * @Date: 2020-02-24 20:10:36
+ */
+
+using System;
+using SimpleJson;
+using UnityEngine;
+using Pomelo.DotNetClient;
+using Battlehub.Dispatcher;
+
+namespace Pomelo.Extend
+{
+    /// <summary>
+    /// connect to server
+    /// </summary> 
+    public class Connector : MonoBehaviour
+    {
+        // should this connector be marked as DontDestroyOnLoad
+        public bool Persist = false;
+
+        // network changed event
+        public event Action<NetWorkState> NetState;
+
+        // net connect proxy
+        PomeloClient m_Conn;
+
+        /// <summary>
+        /// use this for initialization
+        /// </summary>
+        private void Start()
+        {
+            if (Persist)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+            if (FindObjectOfType<Dispatcher>() == null)
+            {
+                gameObject.AddComponent<Dispatcher>();
+            }
+        }
+
+        /// <summary>
+        /// use this for clear work
+        /// </summary>
+        private void OnDestroy()
+        {
+            Disconnect();
+        }
+
+        /// <summary>
+        /// connecting to server and request with default route
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        /// <param name="user">handshake msg for connect</param> 
+        /// <param name="action">default request callback</param>
+        public void Connect(string ip, int port, JsonObject user, Action<JsonObject> action)
+        {
+            if (m_Conn != null)
+            {
+                Debug.Log("the network has been connected!");
+                return;
+            }
+            Debug.Log("connect to ip: " + ip + " port: " + port);
+            m_Conn = new PomeloClient();
+            m_Conn.NetWorkStateChangedEvent += (state) =>
+            {
+                Debug.Log("network state: " + state);
+                if (NetState != null)
+                {
+                    NetState.Invoke(state);
+                }
+            };
+            m_Conn.initClient(ip, port, () =>
+            {
+                m_Conn.connect(user, (rt) =>
+                {
+                    Dispatcher.Current.BeginInvoke(() =>
+                    {
+                        action.Invoke(rt);
+                    });
+                });
+            });
+        }
+
+        /// <summary>
+        /// disconnecting to server
+        /// </summary>
+        public void Disconnect()
+        {  
+            if (m_Conn != null)
+            {
+                m_Conn.disconnect();
+                m_Conn = null;
+            }
+            NetState = null;
+        }
+
+        /// <summary>
+        /// listen msg from server
+        /// </summary>
+        /// <param name="eventName">eg: onReceive</param>
+        /// <param name="action"></param>
+        public void Listen(string eventName, Action<JsonObject> action)
+        {
+            m_Conn.on(eventName, (result) =>
+            {
+                Dispatcher.Current.BeginInvoke(() =>
+                {
+                    action.Invoke(result);
+                });
+            });
+        }
+
+        /// <summary>
+        /// request to server
+        /// </summary>
+        /// <param name="route"></param>
+        /// <param name="data"></param>
+        /// <param name="action"></param>
+        public void Request(string route, JsonObject data, Action<JsonObject> action)
+        {
+            m_Conn.request(route, data, (result) =>
+            {
+                Dispatcher.Current.BeginInvoke(() =>
+                {
+                    action.Invoke(result);
+                });
+            });
+        }
+
+        /// <summary>
+        /// request to server
+        /// </summary>
+        /// <param name="route"></param>
+        /// <param name="action"></param>
+        public void Request(string route, Action<JsonObject> action)
+        {
+            m_Conn.request(route, (result) =>
+            {
+                Dispatcher.Current.BeginInvoke(() =>
+                {
+                    action.Invoke(result);
+                });
+            });
+        }
+
+        /// <summary>
+        /// notify to server
+        /// </summary>
+        /// <param name="route"></param>
+        /// <param name="data"></param>
+        public void Notify(string route, JsonObject data)
+        {
+            m_Conn.notify(route, data);
+        }
+    }
+}
+
---
+++ pomelo-dotnetClient.extend/README.md
@@ -0,0 +1,9 @@
+﻿# aim:      
+extend pomelo-unityclient-socket(version 2020-02-24) for unity.
+    
+# usage:        
+1.put pomelo-dotnetClient and pomelo-dotnetClient.extend folders into your unity project.
+    
+2.create an empty GameObject and add Connector.cs to your it as a Component.  
+    
+3.enjoy it.
